804ea79fc933356f05ebbc0d4121cc9b5522d293
nidhikumari <kumari9936nidhi@gmail.com>
2022-09-03 17:48:00 +0530

    Final change

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-let firstCard = 10;
-let secondCard = 1;
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+
+let cards = []
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.querySelector("#cards-el");
+let playerEl = document.getElementById("player-el");
 
+let player = {
+    name: "Nidhi",
+    chips: 145
+}
+
+playerEl.textContent = player.name + ": $" + player.chips;
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+
+}
 function startGame() {
+    isAlive = true;
+
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 }
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -28,10 +54,15 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    let card = 5;
-    sum += card;
-    renderGame();
+
+    if (isAlive === true && hasBlackJack === true) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+
+        renderGame();
+    }
+
 }
 
 
